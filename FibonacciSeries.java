e98256978828484c57c16f825f593277fe37aa2f
Mahesh Shinde <50023435+maheshshinde54@users.noreply.github.com>
2023-10-28 15:42:45 +0530

    Add day 1 code files to core-java

---
+++ FibonacciSeries.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciSeries
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int sum =0;
+        int prevnum =0;
+        int nextnum = 1;
+        System.out.println("Input a number to find Fibonacci Series");
+        int limit =  sc.nextInt();
+        for( int i = 0; i<limit;i++)
+        {
+            prevnum=nextnum;
+            nextnum = sum;
+            sum = prevnum + nextnum;
+            System.out.println(sum);
+            
+            
+        }
+        sc.close();
+        
+    }
+    
+}
